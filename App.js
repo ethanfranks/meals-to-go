64fa4f4ceb6b629445356a570f955a9e10743e52
Ethan Franks <efranks@bootbarn.com>
2023-05-12 16:22:26 -0500

    SafeAreaView/StatusBar & eslint

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  SafeAreaView,
+  View,
+  Text,
+  Platform,
+  StatusBar,
+} from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+
+const isAndroid = Platform.OS === "android";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.searchContainer}>
+          <Text>search</Text>
+        </View>
+        <View style={styles.listContainer}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: isAndroid ? StatusBar.currentHeight : 0,
+  },
+  searchContainer: {
+    padding: 16,
+    justifyContent: "center",
+    backgroundColor: "green",
+  },
+  listContainer: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
   },
 });
